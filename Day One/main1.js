f9878ee2301a989d3631132cc9035415962142d2
AdodoolaO <58596404+AdodoolaO@users.noreply.github.com>
2023-02-24 13:58:42 +0000

    Update main1.js

--- Day One/main1.js
+++ Day One/main1.js
@@ -1,27 +1,33 @@
 //const {readFile} = require('fs');
 //const path = require ('path');
-import { readFileSync } from "node:fs";
+const fs = require("fs");
 
-const elves = readFileSync("dayOneExample.txt", { encoding: "utf-8" }) // read day??.txt content
-  .replace(/\r/g, "") // remove all \r characters to avoid issues on Windows
-  .trim() // Remove starting/ending whitespace
-  .split("\n\n"); // Split on newline
+const data = fs.readFileSync("./dayOne.txt",  { encoding: "utf-8" })
+.replace(/\r/g, "") // remove all \r characters to avoid issues on Windows
+.trim() // Remove starting/ending whitespace
+.split("\n\n"); // Split on newline
 
+console.log(data)
 
-  function part1() {
-    const calories = elves.map((elf) => {
-      const calories = elf.split("\n").map(Number);
-     return calories.reduce((previous, current) => previous + current, 0); 
-    });
-  
-  console.log(Math.max(...calories));
-  }
+function part1(){
+  const calories = data.map((elf) => {
+   const calories = elf.split("\n").map(Number);
+return calories.reduce((previous, current) => previous + current, 0);   
+  });
+
+ console.log(Math.max(...calories));
+}
+
 function part2(){
 //Find the top three Elves carrying the most Calories
-
+const calories = data.map((elf) => {
+  const calories = elf.split("\n").map(Number);
+return calories.reduce((previous, current) => previous + current, 0);   
+ });
 
 
 
 }
-  
-  part1()
+
+part1()
+part2()
